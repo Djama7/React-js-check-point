017a4c8d532fe371e9634c458f38e19095f984d4
Nguyen Duc MuiDjama7 <jamoussi_amine@hotmail.fr>
2022-08-07 17:05:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+import CollapsibleExample from './compo/navbar' ; 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './compo/form';
+
+
+
+
+  
+
+
+function App() {
+  return (
+    <div>
+      
+    <>
+    
+    <CollapsibleExample/>
+    <BasicExample/>
+  
+
+     
+      
+      
+      
+      
+
+     </>
+     
+
+
+
+
+</div>
+
+    
+     
+      
+    
+      
+        
+      
+      
+      
+      
+    
+   
+  );
+}
+
+export default App;
