153b57739eb4b6b9ca186d41a0099f9e2ae981c5
Djama7 <107061267+Djama7@users.noreply.github.com>
2022-08-08 10:51:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
-
-import './App.css';
-import CollapsibleExample from './compo/navbar' ; 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BasicExample from './compo/form';
-
-
-
-
-  
-
+import "./App.css";
+import CollapsibleExample from "./compo/navbar";
+import BasicExample from "./compo/form";
 
 function App() {
   return (
-    <div>
-      
     <>
-    
-    <CollapsibleExample/>
-    <BasicExample/>
-  
-
-     
-      
-      
-      
-      
-
-     </>
-     
-
-
-
-
-</div>
-
-    
-     
-      
-    
-      
-        
-      
-      
-      
-      
-    
-   
+      <CollapsibleExample />
+      <BasicExample />
+    </>
   );
 }
 
