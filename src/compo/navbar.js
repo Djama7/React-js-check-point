1e1d101a532dd0809fcaee1a3b97e52064bc384a
Djama7 <107061267+Djama7@users.noreply.github.com>
2022-08-08 10:53:52 +0100

    Update navbar.js

--- src/compo/navbar.js
+++ src/compo/navbar.js
@@ -1,10 +1,4 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Button from 'react-bootstrap/Button';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function CollapsibleExample() {
   return (
